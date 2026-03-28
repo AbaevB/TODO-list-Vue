4985eb48102cbb4ebd6c6147366d851fb930f714
Andrey Baev <60654928+AbaevB@users.noreply.github.com>
2026-03-28 16:49:19 +0700

    feat(TodoList.vue): add TodoList template

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,1 +1,28 @@
 <!-- src/components/TodoList.vue -->
+ <script setup>
+import TodoItem from './TodoItem.vue';
+
+ defineProps({
+  todos: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+<template>
+<ul class="todo-list">
+    <li 
+    v-for="todo in todos"
+    :key="todo.id"
+    class="todo-list__item"
+    :class="{'todo-list__item--done' : todo.done}"
+    >
+    <TodoItem 
+    :todo="todo"
+    @toggle="$emit('toggle', todo.id)"
+    @remove="$emit('remove', todo.id)"
+    />
+
+    </li>
+</ul>
+</template>
